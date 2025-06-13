23b5e29bf9f8b31c91503787e994c477f1adde72
Indraja <induponimoni542@gmail.com>
2025-06-13 21:09:00 +0530

    deployment bug

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "https://lms-six-sigma.vercel.app/",
+    origin: "https://lms-six-sigma.vercel.app",
     credentials: true
 }))
 
